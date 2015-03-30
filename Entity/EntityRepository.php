41de62f26605b233a2bfa5fb7c51d7adc9739484
Yoshi Aoki <yoshi@44services.jp>
2015-03-30 19:31:23 +0900

    replace { PACKAGE } with namespace

--- Entity/EntityRepository.php
+++ Entity/EntityRepository.php
@@ -12,7 +12,7 @@ use O3Co\Query\Query\Visitor\FieldResolver\MappedFieldResolver;
  * EntityRepository 
  * 
  * @uses BaseEntityRepository
- * @package { PACKAGE }
+ * @package \O3Co\Query
  * @copyright Copyrights (c) 1o1.co.jp, All Rights Reserved.
  * @author Yoshi<yoshi@1o1.co.jp> 
  * @license MIT
